40ee6d0c5eed477f19a6e9269c255c50d1a9df00
Andrew Berglund <aberglun@redhat.com>
2020-04-07 21:02:26 -0400

    Add an incomplete manifest report

---
+++ kokudaily/sql/incomplete_manifests.sql
@@ -0,0 +1,19 @@
+SELECT    p.uuid as provider_uuid,
+          rm.assembly_id,
+          rm.manifest_creation_datetime,
+          rm.manifest_updated_datetime,
+          rm.billing_period_start_datetime,
+          rm.num_processed_files,
+          rm.num_total_files,
+          rs.report_name,
+          rs.last_started_datetime as report_start_datetime,
+          rs.last_completed_datetime as report_completed_datetime
+FROM      public.reporting_common_costusagereportmanifest AS rm
+JOIN      public.api_provider AS p
+ON        rm.provider_id = p.uuid
+LEFT JOIN public.reporting_common_costusagereportstatus AS rs
+ON        rm.id = rs.manifest_id
+WHERE     num_processed_files != num_total_files
+AND       manifest_creation_datetime >= current_date - INTERVAL '1 day'
+ORDER BY  manifest_creation_datetime DESC
+;
