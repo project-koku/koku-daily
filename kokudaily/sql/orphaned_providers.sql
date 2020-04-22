cb28cab592b24c4c560fb6caad3a5e4bf6a6c1e2
Chris Hambridge <chambrid@redhat.com>
2020-04-22 12:05:30 -0400

    Update SQL join conditions.

--- kokudaily/sql/orphaned_providers.sql
+++ kokudaily/sql/orphaned_providers.sql
@@ -1,9 +1,9 @@
 SELECT cust.account_id,
        t.*
-FROM   public.api_provider t
-       join public.api_sources AS sources
-         ON t.uuid :: text != sources.koku_uuid
-       join public.api_customer AS cust
+FROM   PUBLIC.api_provider t
+       LEFT JOIN PUBLIC.api_sources AS sources
+              ON sources.koku_uuid IS NULL
+       JOIN PUBLIC.api_customer AS cust
          ON t.customer_id = cust.id
 GROUP  BY t.uuid,
           cust.account_id  
