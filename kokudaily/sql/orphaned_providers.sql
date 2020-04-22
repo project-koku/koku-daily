d55310300b0ea11f438d4519bda6c4a388f742af
Chris Hambridge <chambrid@redhat.com>
2020-04-22 12:08:06 -0400

    Fix the where clause.

--- kokudaily/sql/orphaned_providers.sql
+++ kokudaily/sql/orphaned_providers.sql
@@ -1,9 +1,10 @@
 SELECT cust.account_id,
        t.*
-FROM   PUBLIC.api_provider t
-       LEFT JOIN PUBLIC.api_sources AS sources
-              ON sources.koku_uuid IS NULL
-       JOIN PUBLIC.api_customer AS cust
+FROM   public.api_provider t
+       left join public.api_sources AS sources
+              ON t.uuid :: text = sources.koku_uuid
+       join public.api_customer AS cust
          ON t.customer_id = cust.id
+WHERE  sources.koku_uuid IS NULL
 GROUP  BY t.uuid,
-          cust.account_id  
+          cust.account_id
