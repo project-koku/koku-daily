a08e50238cc7442d9cccfa6df3922390d75838a8
Chris Hambridge <chambrid@redhat.com>
2020-04-22 12:44:45 -0400

    Group orphaned providers by provider UUID and customer ID. (#5)
    * Group orphaned providers by provider UUID and customer ID.

--- kokudaily/sql/orphaned_providers.sql
+++ kokudaily/sql/orphaned_providers.sql
@@ -1,7 +1,10 @@
-SELECT t.*,
-       cust.account_id
+SELECT cust.account_id,
+       t.*
 FROM   public.api_provider t
-       join public.api_sources AS sources
-         ON t.uuid :: text != sources.koku_uuid
+       left join public.api_sources AS sources
+              ON t.uuid :: text = sources.koku_uuid
        join public.api_customer AS cust
          ON t.customer_id = cust.id
+WHERE  sources.koku_uuid IS NULL
+GROUP  BY t.uuid,
+          cust.account_id
