fe91c2c59dd1da58d1d4d110c985112eb1a378af
Andrew Berglund <aberglun@redhat.com>
2020-06-16 11:14:38 -0400

    Use status table for manifest updated

--- kokudaily/sql/incomplete_manifests.sql
+++ kokudaily/sql/incomplete_manifests.sql
@@ -4,7 +4,7 @@ SELECT    c.id as customer_id,
           p.type as source_type,
           rm.assembly_id,
           rm.manifest_creation_datetime,
-          rm.manifest_updated_datetime,
+          counts.manifest_updated_datetime,
           rm.manifest_completed_datetime,
           rm.billing_period_start_datetime,
           counts.num_processed_files,
@@ -17,7 +17,8 @@ ON        p.customer_id = c.id
 JOIN (
     SELECT   rm.id,
              count(rs.*) FILTER (WHERE last_completed_datetime IS NOT NULL) AS num_processed_files,
-             count(rs.*) AS num_total_files
+             count(rs.*) AS num_total_files,
+             max(rs.last_completed_datetime) as manifest_updated_datetime
     FROM     public.reporting_common_costusagereportmanifest AS rm
     JOIN     public.reporting_common_costusagereportstatus AS rs
     ON       rm.id = rs.manifest_id
@@ -26,7 +27,7 @@ JOIN (
 ON        rm.id = counts.id
 WHERE     counts.num_processed_files != counts.num_total_files
 AND       rm.manifest_creation_datetime >= current_date - INTERVAL '1 day'
-AND       rm.manifest_updated_datetime < now() - INTERVAL '10 min' -- It has been longer than 10 minutes since we processed anything
+AND       counts.manifest_updated_datetime < now() - INTERVAL '10 min' -- It has been longer than 10 minutes since we processed anything
 ORDER BY  c.id,
           p.type,
           rm.manifest_creation_datetime
