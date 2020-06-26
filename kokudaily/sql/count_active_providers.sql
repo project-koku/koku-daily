81ccf3d8a1321209789a0f6b60c9f1cabde60140
Chris Hambridge <chambrid@redhat.com>
2020-06-26 10:49:25 -0400

    Capture active providers in metric and email. (#10)

---
+++ kokudaily/sql/count_active_providers.sql
@@ -0,0 +1,13 @@
+SELECT    count (DISTINCT t.*),
+          cust.account_id,
+          t.type as source_type
+FROM      PUBLIC.api_provider t
+LEFT JOIN PUBLIC.api_sources AS sources
+ON        t.uuid :: text = sources.koku_uuid
+JOIN      PUBLIC.api_providerstatus AS status
+ON        t.uuid = status.provider_id
+JOIN      PUBLIC.api_customer AS cust
+ON        t.customer_id = cust.id
+WHERE     status.timestamp >= now() - interval '48 HOURS'
+AND       sources.koku_uuid IS NOT NULL
+GROUP BY cust.account_id, t.type
