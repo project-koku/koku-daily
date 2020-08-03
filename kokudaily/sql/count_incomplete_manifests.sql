fe0b35d73161e3aec76e8908c9521d97e9719a0e
Chris Hambridge <chambrid@redhat.com>
2020-08-03 08:49:52 -0400

    Update the count incomplete manifest query.

--- kokudaily/sql/count_incomplete_manifests.sql
+++ kokudaily/sql/count_incomplete_manifests.sql
@@ -6,7 +6,21 @@ JOIN      public.api_provider AS p
 ON        rm.provider_id = p.uuid
 JOIN      public.api_customer AS c
 ON        p.customer_id = c.id
-WHERE     num_processed_files != num_total_files
-AND       manifest_creation_datetime >= current_date - INTERVAL '1 day'
-GROUP BY c.schema_name, p.type
+JOIN (
+    SELECT   rm.id,
+             count(rs.*) FILTER (WHERE last_completed_datetime IS NOT NULL) AS num_processed_files,
+             count(rs.*) AS num_total_files,
+             max(rs.last_completed_datetime) as manifest_updated_datetime
+    FROM     public.reporting_common_costusagereportmanifest AS rm
+    JOIN     public.reporting_common_costusagereportstatus AS rs
+    ON       rm.id = rs.manifest_id
+    GROUP BY rm.id
+) AS counts
+ON        rm.id = counts.id
+WHERE     counts.num_processed_files != counts.num_total_files
+AND       rm.manifest_creation_datetime >= current_date - INTERVAL '1 day'
+AND       counts.manifest_updated_datetime < now() - INTERVAL '10 min' -- It has been longer than 10 minutes since we processed anything
+GROUP BY  c.schema_name,
+          p.type,
+          rm.manifest_creation_datetime
 ;
