f75919104f57c354f22230ca419545f4898f7476
Douglas Curtis <docurtis@redhat.com>
2020-08-06 13:37:12 -0400

    replacing providerstatus

--- kokudaily/sql/count_active_providers.sql
+++ kokudaily/sql/count_active_providers.sql
@@ -1,13 +1,27 @@
-SELECT    count (DISTINCT t.*),
+CREATE TEMPORARY TABLE manifest_temp AS (
+    SELECT  DISTINCT ON(provider_id)
+            provider_id,
+            billing_period_start_datetime,
+            manifest_completed_datetime
+    FROM PUBLIC.reporting_common_costusagereportmanifest
+    ORDER BY provider_id,
+             billing_period_start_datetime
+    DESC NULLS LAST
+);
+
+SELECT    DISTINCT ON(status.provider_id)
+          count (DISTINCT t.*),
           cust.account_id,
           t.type as source_type
 FROM      PUBLIC.api_provider t
 LEFT JOIN PUBLIC.api_sources AS sources
 ON        t.uuid :: text = sources.koku_uuid
-JOIN      PUBLIC.api_providerstatus AS status
+JOIN      manifest_temp AS status
 ON        t.uuid = status.provider_id
 JOIN      PUBLIC.api_customer AS cust
 ON        t.customer_id = cust.id
-WHERE     status.timestamp >= now() - interval '48 HOURS'
+WHERE     status.manifest_completed_datetime >= now() - interval '48 HOURS'
 AND       sources.koku_uuid IS NOT NULL
-GROUP BY cust.account_id, t.type
+GROUP BY cust.account_id, t.type, status.provider_id
+;
+DROP TABLE manifest_temp;
