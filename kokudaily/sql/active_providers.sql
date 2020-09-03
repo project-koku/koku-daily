56e8c8497054cedff87e89eddc7629ef2b5a4bc4
Chris Hambridge <chambrid@redhat.com>
2020-09-03 10:27:53 -0400

    Fix no rows in resultset error.

--- kokudaily/sql/active_providers.sql
+++ kokudaily/sql/active_providers.sql
@@ -1,4 +1,4 @@
-CREATE TEMPORARY TABLE manifest_temp AS (
+WITH cte_manifest_temp AS (
     SELECT  DISTINCT ON(provider_id)
             provider_id,
             id,
@@ -7,18 +7,16 @@ CREATE TEMPORARY TABLE manifest_temp AS (
     ORDER BY provider_id,
              id
     DESC NULLS LAST
-);
-
+)
 SELECT    cust.account_id,
           t.*
 FROM      PUBLIC.api_provider t
 LEFT JOIN PUBLIC.api_sources AS sources
 ON        t.uuid :: text = sources.koku_uuid
-JOIN      manifest_temp AS status
+JOIN      cte_manifest_temp AS status
 ON        t.uuid = status.provider_id
 JOIN      PUBLIC.api_customer AS cust
 ON        t.customer_id = cust.id
 WHERE     status.manifest_completed_datetime >= now() - interval '48 HOURS'
 AND       sources.koku_uuid IS NOT NULL
 ;
-DROP TABLE manifest_temp;
