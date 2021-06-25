e7350e492c4426717d3db595644ac2e0365793ee
Andrew Berglund <aberglun@redhat.com>
2021-06-25 16:57:19 -0400

    Update user count SQL

--- kokudaily/sql/count_filtered_users.sql
+++ kokudaily/sql/count_filtered_users.sql
@@ -3,8 +3,8 @@
                 SELECT DISTINCT t.customer_id,
                                 substring(t.email from '@(.*)$') AS domain
                 FROM            PUBLIC.api_user t
-                WHERE           substring(t.email FROM '@(.*)$') != 'redhat.com' ), filtered_customers AS
-(
+                WHERE           substring(t.email FROM '@(.*)$') != 'redhat.com' ),
+filtered_customers AS (
          SELECT   c.id,
                   c.account_id,
                   cnr.domain
@@ -18,8 +18,16 @@
                                        '6289400',
                                        '6289401')
          GROUP BY c.id,
-                  cnr.domain )
-SELECT count (DISTINCT t.username)
+                  cnr.domain
+),
+cte_active_customers AS (
+    SELECT DISTINCT customer_id
+    FROM PUBLIC.api_provider
+)
+SELECT count(DISTINCT t.username) as total_users,
+    count(DISTINCT t.username) FILTER (WHERE ac.customer_id IS NOT NULL) as users_with_active_customers
 FROM   PUBLIC.api_user t
 JOIN   filtered_customers AS fc
 ON     t.customer_id = fc.id
+LEFT JOIN cte_active_customers AS ac
+ON     t.customer_id = ac.customer_id
