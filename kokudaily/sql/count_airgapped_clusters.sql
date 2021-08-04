6640240ea43471185f2abc3e1bf085ed98a18285
Ashley Aiken <aaiken@redhat.com>
2021-08-04 14:07:56 -0400

    initial sql and reports

---
+++ kokudaily/sql/count_airgapped_clusters.sql
@@ -0,0 +1,13 @@
+SELECT    count (DISTINCT rm.*) filter (WHERE operator_airgapped = true),
+          REPLACE(c.schema_name, 'acct', '') as account_id,
+          p.type as source_type
+FROM      public.reporting_common_costusagereportmanifest AS rm
+JOIN      public.api_provider AS p
+ON        rm.provider_id = p.uuid
+JOIN      public.api_customer AS c
+ON        p.customer_id = c.id
+GROUP BY  c.schema_name,
+          p.type,
+          rm.operator_airgapped,
+          rm.provider_id
+;
