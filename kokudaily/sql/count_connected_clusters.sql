1fac0fd4ab521f56c9d99cbc013ac48ecc06405e
Ashley Aiken <aaiken@redhat.com>
2021-08-06 11:22:20 -0400

    restrict it to one entry per provider

--- kokudaily/sql/count_connected_clusters.sql
+++ kokudaily/sql/count_connected_clusters.sql
@@ -5,12 +5,25 @@ SELECT count (*) as "count",
        rm.cluster_id,
        c.account_id,
        p.type as source_type
-  FROM public.reporting_common_costusagereportmanifest AS rm
+  FROM
+    (SELECT provider_id,
+        row_number() OVER (PARTITION BY provider_id ORDER BY manifest_creation_datetime DESC) as row_number,
+        max(assembly_id) as assembly_id,
+        operator_airgapped,
+        max(operator_version) as operator_version,
+        cluster_id
+    FROM public.reporting_common_costusagereportmanifest AS rm
+    GROUP BY provider_id,
+        operator_airgapped,
+        operator_version,
+        cluster_id,
+        manifest_creation_datetime
+    ) as rm
   JOIN public.api_provider AS p
     ON p.uuid = rm.provider_id
   JOIN public.api_customer AS c
     ON c.id = p.customer_id
- WHERE not coalesce(rm.operator_airgapped, true)
+ WHERE not coalesce(rm.operator_airgapped, true) AND rm.row_number = 1
  GROUP
     BY c.account_id,
        rm.assembly_id,
