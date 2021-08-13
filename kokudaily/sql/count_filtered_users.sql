ee920031a7f4e3500e42fab0221f0ed00bb33236
Andrew Berglund <aberglun@redhat.com>
2021-08-13 15:11:10 -0400

    Value in gauge match name in sql

--- kokudaily/sql/count_filtered_users.sql
+++ kokudaily/sql/count_filtered_users.sql
@@ -24,7 +24,7 @@ cte_active_customers AS (
     SELECT DISTINCT customer_id
     FROM PUBLIC.api_provider
 )
-SELECT count(DISTINCT t.username) FILTER (WHERE ac.customer_id IS NOT NULL) as total_users
+SELECT count(DISTINCT t.username) FILTER (WHERE ac.customer_id IS NOT NULL)
 FROM   PUBLIC.api_user t
 JOIN   filtered_customers AS fc
 ON     t.customer_id = fc.id
