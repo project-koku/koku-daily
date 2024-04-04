966a82a954d84d0683e04dac00bdf4f2be828e70
maskarb <mskarbek@redhat.com>
2024-04-04 12:04:48 -0400

    collect schema level metrics

--- kokudaily/sql/key_metrics/cust_openshift_infra_data.sql
+++ kokudaily/sql/key_metrics/cust_openshift_infra_data.sql
@@ -1,26 +1,27 @@
 SELECT
+    row_number() OVER () AS id,
     to_char(DATE_TRUNC('month', date), 'YYYY-MM') AS month,
-    SUM(cluster_count) AS total_cluster_count,
-    SUM(node_count) AS total_node_count,
-    SUM(infra_node_count) AS total_infra_node_count,
-    SUM(control_plane_node_count) AS total_control_plane_node_count,
-    SUM(worker_node_count) AS total_worker_node_count,
-    SUM(infra_node_cpu_cores) AS total_infra_node_cpu_cores,
-    SUM(control_plane_node_cpu_cores) AS total_control_plane_node_cpu_cores,
-    SUM(worker_node_cpu_cores) AS total_worker_node_cpu_cores,
-    SUM(infra_node_mem_gb) AS total_infra_node_mem_gb,
-    SUM(control_plane_node_mem_gb) AS total_control_plane_node_mem_gb,
-    SUM(worker_node_mem_gb) AS total_worker_node_mem_gb,
-    SUM(pvc_count) AS total_pvc_count,
-    SUM(cluster_capacity_cores) AS total_cluster_capacity_cores,
+    MAX(cluster_count) AS total_cluster_count,
+    MAX(node_count) AS total_node_count,
+    MAX(infra_node_count) AS total_infra_node_count,
+    MAX(control_plane_node_count) AS total_control_plane_node_count,
+    MAX(worker_node_count) AS total_worker_node_count,
+    MAX(infra_node_cpu_cores) AS total_infra_node_cpu_cores,
+    MAX(control_plane_node_cpu_cores) AS total_control_plane_node_cpu_cores,
+    MAX(worker_node_cpu_cores) AS total_worker_node_cpu_cores,
+    MAX(infra_node_mem_gb) AS total_infra_node_mem_gb,
+    MAX(control_plane_node_mem_gb) AS total_control_plane_node_mem_gb,
+    MAX(worker_node_mem_gb) AS total_worker_node_mem_gb,
+    MAX(pvc_count) AS total_pvc_count,
+    MAX(cluster_capacity_cores) AS total_cluster_capacity_cores,
     SUM(cluster_capacity_core_hours) AS total_cluster_capacity_core_hours,
-    SUM(cluster_capacity_memory_gb) AS total_cluster_capacity_memory_gb,
+    MAX(cluster_capacity_memory_gb) AS total_cluster_capacity_memory_gb,
     SUM(cluster_capacity_memory_gb_hours) AS total_cluster_capacity_memory_gb_hours,
-    SUM(volume_request_gb) AS total_volume_request_gb,
+    MAX(volume_request_gb) AS total_volume_request_gb,
     SUM(volume_request_gb_mo) AS total_volume_request_gb_mo,
-    SUM(pvc_capacity_gb) AS total_pvc_capacity_gb,
+    MAX(pvc_capacity_gb) AS total_pvc_capacity_gb,
     SUM(pvc_capacity_gb_mo) AS total_pvc_capacity_gb_mo
 FROM __cust_openshift_infra_report
-GROUP BY DATE_TRUNC('month', date)
-ORDER BY DATE_TRUNC('month', date)
+GROUP BY schema, DATE_TRUNC('month', date)
+ORDER BY schema, DATE_TRUNC('month', date)
 ;
